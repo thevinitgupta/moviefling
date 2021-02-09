061ad59e744fea5779932bf14fa8e30c3ac3ca88
Vinit Gupta <vinitman35@gmail.com>
2021-02-09 18:30:26 +0530

    Separate the working for movies and tv shows

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Main.css";
 import Search from './Search';
 
 function Main() {
+    const [searchFor,setSearchFor] = useState("movie");
+    
+
+    function handleSearchKeyword(element,keyword){
+        if(keyword){
+            element.target.parentNode.childNodes.forEach(entry => {
+                if(entry===element.target) {
+                    element.target.classList.add("active")
+                }
+                else {
+                    entry.classList.remove("active")
+                }
+            })
+            setSearchFor(keyword);
+        }
+    }
     return (
         <div className="main">
             <div className="main__head">
-                <div className="main__head__movie active">Movies</div>
-                <div className="main__head__shows">Shows</div>
+                <div className="main__head__movie active" onClick={(e)=>{handleSearchKeyword(e,e.target.classList["0"].substr(12,5))}}>Movies</div>
+                <div className="main__head__tv" onClick={(e)=>{handleSearchKeyword(e,e.target.classList["0"].substr(12,2))}}>Shows</div>
             </div>
             <div className="main__body">
-                <Search/>
+                <Search searchFor={searchFor}/>
             </div>
         </div>
     )
--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,17 @@
 import React from 'react'
-
-function Search() {
+import "./Search.css"
+function Search({searchFor}) {
+    const apiKey = process.env.REACT_APP_TMDB_API_KEY;
+    function handleSearch(){
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=spider&page=1&include_adult=true`).then(res => res.json()).then(resp=>{
+            console.log(resp)
+        })
+    }
+    let searchBtnClass = `search__btn search__btn__${searchFor}`;
     return (
         <div className="search">
-           <form>
                <input id="keyword" className="search__input" placeholder="Enter movie name or keyword"/>
-               <button className="search__btn" id="search_keyword">Search</button>
-           </form> 
+               <button className={searchBtnClass} id="search_submit" onClick={handleSearch}>Search</button>
         </div>
     )
 }
