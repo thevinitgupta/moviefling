3330d914b6e99a47dcbb21b65715dd9493137f18
Vinit Gupta <vinitman35@gmail.com>
2021-02-12 21:49:31 +0530

    Finalised basic search

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="app">
       <div className="app__head">
-        <span>movie flix</span>
+        <span className="app__name">movie fling</span>
+        <div className="acknowledgement"><img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" alt="tmdb
+        _logo"></img> </div>
       </div>
       <Main />
     </div>
