7d385b7f4ac108bf59f78ace3fd57daac3fb6ffa
Vinit Gupta <vinitman35@gmail.com>
2021-02-26 11:31:41 +0530

    Made the details page more responsive

--- src/Profiles.js
+++ src/Profiles.js
@@ -9,10 +9,41 @@ import "slick-carousel/slick/slick-theme.css";
 function Profiles({profilesDetails}) {
     const settings = {
         dots: false,
+        lazyLoad: true,
         infinite: true,
+        autoplay: true,
         speed: 1000,
+        autoplaySpeed: 4000,
         slidesToShow: 3,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 850,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ],
+          adaptiveHeight: true,
+          cssEase: "ease-in"
       };
     return (
         
